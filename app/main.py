8aa57fb402263f08acacdda4b3130573c96490fb
u-masao <4973920+u-masao@users.noreply.github.com>
2024-06-08 00:38:23 +0900

    [update] 設定フィールド名の衝突を回避。POSTに変更

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 from redis import Redis
 from sentence_transformers import SentenceTransformer
 
@@ -8,21 +9,25 @@ from .settings import Settings
 app = FastAPI()
 settings = Settings()
 redis = Redis(host=settings.redis_host, port=settings.redis_port, db=0)
-embedding_model = SentenceTransformer(settings.model_name)
+embedding_model = SentenceTransformer(settings.hf_model_name)
+
+
+class InputText(BaseModel):
+    text: str
 
 
 @app.post("/v1/embeddings")
-async def get_embeddings(input: str, model: str):
-    print(model, input)
-    cache_key = f"embeddings:{input}"
+async def get_embeddings(input_text: InputText = Body(...)):
+    cache_key = f"embeddings:{settings.hf_model_name}:{input_text.text}"
     cached_result = redis.get(cache_key)
 
+    # cache hit
     if cached_result:
         return JSONResponse(
             content={"embedding": eval(cached_result.decode())}
         )
 
-    embedding = embedding_model.encode(input).tolist()
+    # cache miss
+    embedding = embedding_model.encode(input_text.text).tolist()
     redis.set(cache_key, str(embedding))
-
     return JSONResponse(content={"embedding": embedding})
--- app/settings.py
+++ app/settings.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    model_name: str = "intfloat/multilingual-e5-small"
+    hf_model_name: str = "intfloat/multilingual-e5-small"
     redis_host: str = "redis"
     redis_port: int = 6379
 
