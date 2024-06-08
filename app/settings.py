66dc46a3ce4fd15a4acb51c55b0b4d19bb6f5707
u-masao <4973920+u-masao@users.noreply.github.com>
2024-06-08 19:01:19 +0900

    [update] max_length を追加

--- app/settings.py
+++ app/settings.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     hf_model_name: str = "intfloat/multilingual-e5-small"
+    hf_max_length: int = 512
     redis_host: str = "redis"
     redis_port: int = 6379
 
